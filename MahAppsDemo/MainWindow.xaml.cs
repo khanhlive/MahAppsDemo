2839c2813ee70b2fc4f11e0ac6d644f1778a96d9
khanhlive <khanhngdinh@outlook.com>
2017-08-01 08:10:39 +0700

    updated 01/08/2017

---
+++ MahAppsDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace MahAppsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            var db = new Models.ISOEntities();
+            this.Admins = db.ChiPhis.ToList();
+            InitializeComponent();
+            
+        }
+
+        public List<Models.ChiPhi> Admins
+        {
+            get;set;
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            LoginDialogData result = await this.ShowLoginAsync("Thông báo", "This is message");
+            
+        }
+    }
+}
